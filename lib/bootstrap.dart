13776d5c4eb07dd5d264b3f15003376ad14cbd36
samax <samax.zer@gmail.com>
2024-03-16 20:01:37 +0300

    refactored bootstrap

--- lib/bootstrap.dart
+++ lib/bootstrap.dart
@@ -1,24 +1,39 @@
 import 'dart:async';
 import 'dart:developer';
 
-import 'package:bloc/bloc.dart';
+// import 'package:bloc/bloc.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_todos/app/app.dart';
-import 'package:flutter_todos/app/app_bloc_observer.dart';
+import 'package:flutter_todos/providers/providers.dart';
 import 'package:todos_api/todos_api.dart';
 import 'package:todos_repository/todos_repository.dart';
 
 void bootstrap({required TodosApi todosApi}) {
   FlutterError.onError = (details) {
-    log(details.exceptionAsString(), stackTrace: details.stack);
+    log(
+      details.exceptionAsString(),
+      stackTrace: details.stack,
+    );
   };
 
-  Bloc.observer = const AppBlocObserver();
+  // Bloc.observer = const AppBlocObserver();
 
   final todosRepository = TodosRepository(todosApi: todosApi);
 
   runZonedGuarded(
-    () => runApp(App(todosRepository: todosRepository)),
-    (error, stackTrace) => log(error.toString(), stackTrace: stackTrace),
+    () => runApp(
+      ProviderScope(
+        overrides: [
+          todosRepositoryProvider.overrideWithValue(todosRepository),
+        ],
+        // child: App(todosRepository: todosRepository),
+        child: const App(),
+      ),
+    ),
+    (error, stackTrace) => log(
+      error.toString(),
+      stackTrace: stackTrace,
+    ),
   );
 }
