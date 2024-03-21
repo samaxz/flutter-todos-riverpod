8fb755cbbea90b887fb3f9087862fa694d6ea0e2
shamil <samax.zer@gmail.com>
2024-03-21 20:17:15 +0300

    removed comments from app_test.dart

--- test/app_test.dart
+++ test/app_test.dart
@@ -5,21 +5,15 @@ import 'package:flutter_todos/app/app.dart';
 import 'package:flutter_todos/home/home.dart';
 import 'package:flutter_todos/providers/providers.dart';
 import 'package:flutter_todos/theme/theme.dart';
-import 'package:local_storage_todos_api/local_storage_todos_api.dart';
 import 'package:mocktail/mocktail.dart';
-import 'package:todos_repository/todos_repository.dart';
 
 import 'helpers/helpers.dart';
 
 void main() {
-  // late TodosRepository todosRepository;
-  late MockTodosRepository todosRepository;
-
   ProviderContainer createProviderContainer() {
     final container = ProviderContainer(
       overrides: [
         todosRepositoryProvider.overrideWithValue(MockTodosRepository()),
-        // todosRepositoryProvider.overrideWithValue(MockFakeTodosRepository()),
       ],
     );
     // this throws, since it can only be used inside tests
@@ -28,29 +22,17 @@ void main() {
   }
 
   setUp(() {
-    // todosRepository = MockTodosRepository();
-    todosRepository = MockTodosRepository();
-
     when(
       () => createProviderContainer().read(todosRepositoryProvider).getTodos(),
     ).thenAnswer((_) => const Stream.empty());
-    // ).thenAnswer((_) => null);
   });
 
   group('App', () {
     testWidgets('renders AppView', (tester) async {
-      // TODO remove these, as they prevent the test from going
-      // final todosApi = LocalStorageTodosApi(
-      //   plugin: await SharedPreferences.getInstance(),
-      // );
-      // final todosRepository = TodosRepository(todosApi: todosApi);
       await tester.pumpWidget(
         ProviderScope(
           overrides: [
-            // todosRepositoryProvider.overrideWithValue(MockTodosRepository()),
             todosRepositoryProvider.overrideWithValue(FakeTodosRepository()),
-            // todosRepositoryProvider.overrideWithValue(MockFakeTodosRepository()),
-            // todosRepositoryProvider.overrideWithValue(todosRepository),
           ],
           child: App(),
         ),
@@ -65,9 +47,7 @@ void main() {
       await tester.pumpWidget(
         ProviderScope(
           overrides: [
-            // todosRepositoryProvider.overrideWithValue(MockTodosRepository()),
             todosRepositoryProvider.overrideWithValue(FakeTodosRepository()),
-            // todosRepositoryProvider.overrideWithValue(MockFakeTodosRepository()),
           ],
           child: App(),
         ),
@@ -84,9 +64,7 @@ void main() {
       await tester.pumpWidget(
         ProviderScope(
           overrides: [
-            // todosRepositoryProvider.overrideWithValue(MockTodosRepository()),
             todosRepositoryProvider.overrideWithValue(FakeTodosRepository()),
-            // todosRepositoryProvider.overrideWithValue(MockFakeTodosRepository()),
           ],
           child: App(),
         ),
