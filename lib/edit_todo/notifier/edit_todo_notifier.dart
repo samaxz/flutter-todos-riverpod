63feea96419f9a8a23baeb13009722dafa7900b9
samax <samax.zer@gmail.com>
2024-03-25 20:42:18 +0300

    updated edit_todo_notifier.dart

--- lib/edit_todo/notifier/edit_todo_notifier.dart
+++ lib/edit_todo/notifier/edit_todo_notifier.dart
@@ -1,141 +1,27 @@
 import 'package:equatable/equatable.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter_todos/providers/providers.dart';
+import 'package:mocktail/mocktail.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:todos_repository/todos_repository.dart';
-import 'package:mocktail/mocktail.dart';
 
 part 'edit_todo_notifier.g.dart';
 part 'edit_todo_state.dart';
 
-// TODO remove this
-class AltMockEditTodoNotifier extends EditTodoNotifier with Mock {
-  // @override
-  // // EditTodoState get state => super.state;
-  // EditTodoState get state => EditTodoState();
-  // // EditTodoState get state {
-  // //   // this causes stack overflow
-  // //   // debugPrint('$state getter inside mock notifier');
-  // //   return EditTodoState();
-  // // }
-  //
-  // @override
-  // set state(EditTodoState value) {
-  //   super.state = value;
-  //   // this causes stack overflow
-  //   // state = value;
-  //   debugPrint('$state setter inside mock notifier');
-  // }
-}
-
-// this throws
-// class SomeMock extends Mock implements EditTodoNotifier {}
-
-// could theoretically use this too
-// class SomeMock extends EditTodoNotifier {
-//   set debugState(EditTodoState value) {
-//     state = value;
-//   }
-// }
-//
-// final someMockNP = StateNotifierProvider<EditTodoNotifier, EditTodoState>((ref) => SomeMock());
-
-// TODO remove this
-// final mockEditTodoNotifier = MockEditTodoNotifier();
-
-// TODO remove this
 // for unit testing
-class MockEditTodoNotifier extends _$EditTodoNotifier with Mock implements EditTodoNotifier {
-  // this works
-  // without this, i'm getting ```type 'Null' is not a subtype of type 'EditTodoState'```
-  // @override
-  // EditTodoState build({Todo? initialTodo}) {
-  //   // debugPrint('$state inside mock notifier');
-  //   return EditTodoState(
-  //     initialTodo: initialTodo,
-  //     title: initialTodo?.title ?? '',
-  //     description: initialTodo?.description ?? '',
-  //   );
-  //   // return super.noSuchMethod(
-  //   //   Invocation.method(
-  //   //     #build,
-  //   //     [],
-  //   //   ),
-  //   //   // returnValue: Future.value([]),
-  //   // );
-  // }
-  //
-  // //
-  // @override
-  // // EditTodoState get state => super.state;
-  // EditTodoState get state {
-  //   // this causes stack overflow
-  //   // debugPrint('$state getter inside mock notifier');
-  //   return EditTodoState();
-  // }
-  //
-  // @override
-  // set state(EditTodoState value) {
-  //   super.state = value;
-  //   // this causes stack overflow
-  //   // state = value;
-  //   debugPrint('$state setter inside mock notifier');
-  // }
-  //
-  // @override
-  // Future<void> submitTodo() async {
-  //   // await Future.value();
-  //   // debugPrint('submitTodo() called inside mock notifier');
-  //   // this should be overridden, so, no worries (hopefully)
-  //   await ref.read(todosRepositoryProvider).saveTodo(Todo(title: ''));
-  // }
-
-  // @override
-  // EditTodoState setTodo(Todo todo) {
-  //   return EditTodoState(
-  //     initialTodo: todo,
-  //     title: todo.title,
-  //     description: todo.description,
-  //   );
-  // }
-  //
-  // @override
-  // // void setNewState(Todo todo) {
-  // void setNewState(EditTodoState newState) {
-  //   // state = state.copyWith(
-  //   //   initialTodo: todo,
-  //   //   title: todo.title,
-  //   //   description: todo.description,
-  //   // );
-  //   // state = EditTodoState(
-  //   //   initialTodo: todo,
-  //   //   title: todo.title,
-  //   //   description: todo.description,
-  //   // );
-  //   state = newState;
-  // }
-
-  // @override
-  // void setNewState(EditTodoState newState) {
-  //   state = newState;
-  //   debugPrint('$state inside mock notifier after setNewState');
-  // }
-}
+class MockEditTodoNotifier extends _$EditTodoNotifier with Mock implements EditTodoNotifier {}
 
 // overriding dependencies for testing purposes and to explicitly list all the dependencies
 @Riverpod(dependencies: [todosRepository])
 class EditTodoNotifier extends _$EditTodoNotifier {
   @override
   // it has a default positioned value of null
+  // this is the default value returned by the notifier
   EditTodoState build({Todo? initialTodo}) {
-    // debugPrint('$state inside original notifier');
     return EditTodoState(
       initialTodo: initialTodo,
-      title: initialTodo?.title ?? '',
-      description: initialTodo?.description ?? '',
+      title: '',
+      description: '',
     );
-    // this is done for testing purposes
-    throw UnimplementedError();
   }
 
   void changeTitle(String? newTitle) {
@@ -167,26 +53,4 @@ class EditTodoNotifier extends _$EditTodoNotifier {
       state = state.copyWith(status: EditTodoStatus.failure);
     }
   }
-
-  // used to set state in tests
-  // TODO remove this
-  EditTodoState setTodo(Todo todo) {
-    state = state.copyWith(
-      initialTodo: todo,
-      title: todo.title,
-      description: todo.description,
-    );
-    return state;
-  }
-
-  // void setNewState(Todo todo) {
-  //   state = state.copyWith(
-  //     initialTodo: todo,
-  //     title: todo.title,
-  //     description: todo.description,
-  //   );
-  // }
-
-  // TODO remove this
-  void setNewState(EditTodoState newState) => state = newState;
 }
