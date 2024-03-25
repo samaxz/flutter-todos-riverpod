51b6c36643079819e6e7fba806dbb00f8832655c
samax <samax.zer@gmail.com>
2024-03-25 12:15:40 +0300

    moved todo mock to providers.dart

--- lib/providers/providers.dart
+++ lib/providers/providers.dart
@@ -1,5 +1,6 @@
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:todos_repository/todos_repository.dart';
+import 'package:mocktail/mocktail.dart';
 
 part 'providers.g.dart';
 
@@ -7,3 +8,35 @@ part 'providers.g.dart';
 TodosRepository todosRepository(TodosRepositoryRef ref) {
   throw UnimplementedError();
 }
+
+// this doesn't work with widget testing, but does with unit testing
+class MockTodosRepository extends Mock implements TodosRepository {
+  // this throws
+  // Stream<List<Todo>> getTodos() => Stream.empty();
+
+  // this throws ```Used on a non-mocktail object```
+  // @override
+  // Future<void> saveTodo(Todo todo) => Future.value(todo);
+}
+
+// this works with widget testing
+class FakeTodosRepository implements TodosRepository {
+  @override
+  Future<int> clearCompleted() => Future.value(42);
+
+  @override
+  Future<int> completeAll({required bool isCompleted}) => Future.value(42);
+
+  @override
+  Future<void> deleteTodo(String id) => Future.value(id);
+
+  @override
+  Stream<List<Todo>> getTodos() => Stream.empty();
+
+  @override
+  Future<void> saveTodo(Todo todo) => Future.value(todo);
+}
+
+// this doesn't work with widget testing
+// TODO remove this
+// class MockFakeTodosRepository extends Mock implements FakeTodosRepository {}
