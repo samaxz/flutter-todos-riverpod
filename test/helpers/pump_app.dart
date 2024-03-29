62ce5a03d28a0a8b5989697a153a9bc49373b01b
samax <samax.zer@gmail.com>
2024-03-29 19:55:49 +0300

    updated pump_app.dart

--- test/helpers/pump_app.dart
+++ test/helpers/pump_app.dart
@@ -2,23 +2,35 @@ import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_todos/edit_todo/notifier/edit_todo_notifier.dart';
 import 'package:flutter_todos/l10n/l10n.dart';
+import 'package:flutter_todos/providers/providers.dart';
 import 'package:todos_repository/todos_repository.dart';
 
 extension PumpApp on WidgetTester {
   Future<void> pumpApp(
     Widget widget, {
-    TodosRepository? todosRepository,
+    TodosRepository? mockTodosRepository,
+    Todo? initialTodo,
+    MockEditTodoNotifier? mockEditTodoNotifier,
   }) {
     return pumpWidget(
       ProviderScope(
+        overrides: [
+          if (mockTodosRepository != null)
+            todosRepositoryProvider.overrideWith((ref) => mockTodosRepository),
+          if (mockEditTodoNotifier != null)
+            editTodoNotifierProvider(initialTodo: initialTodo).overrideWith(
+              () => mockEditTodoNotifier,
+            ),
+        ],
         child: MaterialApp(
           localizationsDelegates: const [
             AppLocalizations.delegate,
             GlobalMaterialLocalizations.delegate,
           ],
           supportedLocales: AppLocalizations.supportedLocales,
-          home: Scaffold(body: widget),
+          home: widget,
         ),
       ),
     );
@@ -26,11 +38,13 @@ extension PumpApp on WidgetTester {
 
   Future<void> pumpRoute(
     Route<dynamic> route, {
-    TodosRepository? todosRepository,
+    TodosRepository? mockTodosRepository,
+    Todo? initialTodo,
   }) {
     return pumpApp(
       Navigator(onGenerateRoute: (_) => route),
-      todosRepository: todosRepository,
+      mockTodosRepository: mockTodosRepository,
+      initialTodo: initialTodo,
     );
   }
 }
